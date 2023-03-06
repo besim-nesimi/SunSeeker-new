1969bf1e952b5368d67f3880a6f0b3c6a9e4ac35
bestronaut <nesimibesim@outlook.com>
2023-03-06 09:47:25 +0100

    Starting with API caller / helper.

--- Api/ApiHelper.cs
+++ Api/ApiHelper.cs
@@ -2,5 +2,25 @@
 {
     public class ApiHelper
     {
+        private readonly IConfiguration config;
+
+        public static HttpClient HttpClient { get; set; } = new();
+
+        public ApiHelper(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        // Sätt bas address / grund- inställningar
+        public static void InitializeClient() 
+        {
+            HttpClient.BaseAddress = new Uri("https://api.weatherstack.com/");
+        }
+
+        // Metoden visar aktuella vädret för den stad som valts / skickats till metoden.
+        public void GetCurrentWeather(string city)
+        {
+            var response = HttpClient.GetAsync($"current?access_key{config["AccessKey"]}&query={city.ToLower()}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//var accessKey = builder.Configuration["AccessKey"];
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
